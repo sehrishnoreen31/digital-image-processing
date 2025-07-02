794b28f7b9194c0f068fc1f5cd87eee8c7b8fedd
Sehrish Noreen <sehrishnoreen31@gmail.com>
2025-07-02 18:15:54 +0500

    ImageOps

---
+++ 02_ImageOps.py
@@ -0,0 +1,14 @@
+from PIL import Image, ImageOps
+
+my_image = Image.open('Wallapers\sd (11).jpg')
+# my_image.show()
+
+# convert to gray scale
+gray_scale_image = ImageOps.grayscale(my_image)
+# gray_scale_image.show()
+
+
+equalized_image = ImageOps.equalize(my_image)
+equalized_image.show()
+
+
